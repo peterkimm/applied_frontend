35bab942f68c878a6af66ba3c4935cecb5425f18
Peter <peterkimm35@gmail.com>
2022-06-23 10:32:31 -0700

    navbar done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,50 @@
 import { Link } from "react-router-dom";
 import { login, logout } from "../services/firebase";
+import { IoClipboardOutline } from "react-icons/io5";
+import { IoApps } from "react-icons/io5";
+import { IoBagOutline } from "react-icons/io5";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
+import { IconContext } from "react-icons";
+
 
 const Header = (props) => {
   return (
+    <IconContext.Provider value = {{ 
+        color: "black", 
+        size: "1.7em",
+        className: "allIcons",
+        
+        
+
+
+        }} >
     <nav className="navbar navbar-brand shadow-sm p-1 mb-4">
     <Link to='/' className="navbar-brand">
         <img src="https://github.com/peterkimm/applied_frontend/blob/master/src/images/applied-logo.png?raw=true" height={40} alt="applied logo"/>    </Link>
-    <Link to='/'>
-        <div>Dashboard</div>
+    <Link to='/' className="Link">
+        <IoClipboardOutline className="icon"/>
+        <div className="text dashIcon">Dashboard</div>
     </Link>
-     <Link to='/jobapplications'>
-        <div>My Jobs</div>
+     <Link to='/jobapplications' className="Link">
+         <IoApps className="icon"/>
+        <div className="text">My Jobs</div>
     </Link>
-    <Link to='/create'>
-        <div>New Job</div>
+    <Link to='/create' className="Link">
+        <IoBagOutline className="icon"/>
+        <div className="text">New Job</div>
     </Link>
    
-    <ul className="login">
+    <ul className="login Link">
+        <IoArrowForwardCircleOutline />
         {
-            props.user
-            ? <li onClick={logout}>Logout</li>
-            : <li onClick={login}>Login</li>
+            props.user 
+            ? <li onClick={logout} className="text log">Logout</li>
+            : <li onClick={login} className="text log">Login</li>
             
         }
     </ul>
     </nav>
+    </IconContext.Provider>
   )
 }
 
